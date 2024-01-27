d8b58b99c50836c2bdb940a7a5da402e35ba40fa
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2024-01-27 12:27:08 +0530

    Update formatting

--- src/app/api/v1/me/route.ts
+++ src/app/api/v1/me/route.ts
@@ -4,6 +4,7 @@ import { auth } from '@clerk/nextjs';
 
 export async function GET(request: Request) {
   const { userId } = auth();
+
   if (!userId) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
 
   const prisma = new PrismaClient();
